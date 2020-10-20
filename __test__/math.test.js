3b853790ed2adcaf766b9bad3ba656949e8c0d95
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-20 07:53:46 -0500

    all the test working

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -4,7 +4,13 @@ describe("mathematical calculations", () => {
   test("addition test", () => {
     expect(add(1, 1)).toBe(2);
   });
+  test("subtract test", () => {
+    expect(subtract(3, 1)).toBe(2);
+  });
   test("multiply test", () => {
-    expect(add(2, 2)).toBe(4);
+    expect(multiply(2, 2)).toBe(4);
+  });
+  test("multiply test", () => {
+    expect(divide(2, 2)).toBe(1);
   });
 });
