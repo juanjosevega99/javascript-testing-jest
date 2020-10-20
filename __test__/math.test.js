870b371d7b7932ba52042207974af96bfc3383f9
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-20 07:51:32 -0500

    multiply test

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -4,4 +4,7 @@ describe("mathematical calculations", () => {
   test("addition test", () => {
     expect(add(1, 1)).toBe(2);
   });
+  test("multiply test", () => {
+    expect(add(2, 2)).toBe(4);
+  });
 });
