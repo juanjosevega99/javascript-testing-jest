8b628409f689dee9721ea163516b3c28af4ebf36
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-23 22:06:37 -0500

    [TEST] exceptions working

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,28 @@ describe("it is time for snapshots", () => {
   test("Snapshot", () => {
     expect(getCharacter(rick)).toMatchSnapshot();
   });
+
+  test("fail snapshot", () => {
+    const user = {
+      id: Math.floor(Math.random() * 100),
+      name: "Juan Jose Vega",
+      headline: "Full Stack Developer",
+      createdAt: new Date(),
+    };
+
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number),
+      createdAt: expect.any(Date),
+    });
+  });
+
+  test("we have an exception", () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      name: "Juan Jose Vega",
+    };
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number),
+    });
+  });
 });
